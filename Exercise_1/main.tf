9259a9b32d62d2c80e230fb96eaef63e267928af
Mikisho <mikiye.82@gmail.com>
2020-06-03 10:04:59 -0500

    provision-ec2-instances

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,6 +1,6 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  region = "us-east-1"
+  region = "${var.region}"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
