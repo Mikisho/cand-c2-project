68284eaf361a065f4d653335477ec7b083119c4d
Mikisho <mikiye.82@gmail.com>
2020-06-03 14:05:58 -0500

    adding-projfiles

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,6 +1,6 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  region = "${var.region}"
+  region = "us-east-1"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
