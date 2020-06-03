9b9e22d128329eb4b50b36c788a66468f9dac485
Mikisho <mikiye.82@gmail.com>
2020-06-02 21:49:24 -0500

    provision-ec2-instances

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,26 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  region = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "udacity_t2" {
+  count = 4
+  ami = "ami-09d95fab7fff3776c"
+  subnet_id = "subnet-08bfeacd6c020fb05"
+  instance_type = "t2.micro"
+  tags ={
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "udacity_m4" {
+  count = 2
+  ami = "ami-09d95fab7fff3776c"
+  subnet_id = "subnet-08bfeacd6c020fb05"
+  instance_type = "m4.large"
+  tags ={
+    Name = "Udacity M4"
+  }
+}
